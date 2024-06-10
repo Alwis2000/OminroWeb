1d95ef4a59ba31a1da16ea73737d70862f21454c
Alwis2000 <theonlyalwiz2@gmail.com>
2024-06-10 18:41:04 +0530

    Git upload

---
+++ tabs.js
@@ -0,0 +1,51 @@
+function openPage(evt, page) {
+      // Declare all variables
+      var i, tabcontent, tablinks;
+    
+      // Get all elements with class="tabcontent" and hide them
+      pagecontent = document.getElementsByClassName("pagecontent");
+      for (i = 0; i < pagecontent.length; i++) {
+        pagecontent[i].style.display = "none";
+      }
+
+      pagelinks = document.getElementsByClassName("pagelinks");
+      for (i = 0; i < pagelinks.length; i++) {
+        pagelinks[i].className = pagelinks[i].className.replace(" active", "");
+      }
+    
+      // Get all elements with class="tablinks" and remove the class "active"
+      // tablinks = document.getElementsByClassName("tablinks");
+      // for (i = 0; i < tablinks.length; i++) {
+      //   tablinks[i].className = tablinks[i].className.replace(" active", "");
+      // }
+    
+      // Show the current tab, and add an "active" class to the button that opened the tab
+      document.getElementById(page).style.display = "block";
+      evt.currentTarget.className += " active";
+}
+
+
+
+
+// Add event listener
+document.addEventListener("mousemove", parallax);
+// parallax();
+console.log(elem);
+
+// Magic happens here
+function parallax(e) {
+  let _w = window.innerWidth/2;
+  let _h = window.innerHeight/2;
+  let _mouseX = e.clientX;
+  let _mouseY = e.clientY;
+  let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
+  let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
+  // let x = `${_depth2}, ${_depth1}`;
+  
+  // document.getElementById("Home").style.backgroundPosition = x;
+  document.getElementById("Home").style.setProperty("--x", `${_depth1}`);
+  document.getElementById("Home").style.setProperty("--y", `${_depth2}`);
+
+  console.log(getComputedStyle(document.body).getPropertyValue('--x'));
+}
+
