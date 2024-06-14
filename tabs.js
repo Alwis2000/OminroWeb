44cccb5018dea4627ddfad64b9c83034df8b0fbf
Alwis2000 <theonlyalwiz2@gmail.com>
2024-06-14 17:52:13 +0530

    npm remove

--- tabs.js
+++ tabs.js
@@ -39,8 +39,12 @@ function parallax(e) {
   let _h = window.innerHeight / 2;
   let _mouseX = e.clientX;
   let _mouseY = e.clientY;
-  let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
-  let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
+  let _depth1 = `${50 - (_mouseX - _w) * 0.0075}% ${
+    50 - (_mouseY - _h) * 0.0075
+  }%`;
+  let _depth2 = `${50 - (_mouseX - _w) * 0.015}% ${
+    50 - (_mouseY - _h) * 0.015
+  }%`;
   // let x = `${_depth2}, ${_depth1}`;
 
   // document.getElementById("Home").style.backgroundPosition = x;
